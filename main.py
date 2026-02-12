74a163521252822ecec276814caabd9583fb998a
gff <adsf>
2026-02-12 11:24:29 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def sub(a: int, b: int) -> int:
+    return a - b
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+def main():
+    sum_res: int = sum(7, 3)
+    print(sum_res)
+
+
+if __name__ == '__main__':
+    main()
